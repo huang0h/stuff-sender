8e3b8c0b87c63a503a0e2c7c03324f2f012290ad
huang0h <averyhuang2003@gmail.com>
2025-07-15 17:57:04 -0400

    backend port env

--- backend/main.ts
+++ backend/main.ts
@@ -2,9 +2,12 @@ import express from "express";
 import WebSocket, { WebSocketServer } from "ws";
 import http from "http";
 import { ItemMessage, MessageTypes } from "../types/types";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
